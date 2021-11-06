51b3aa2b005e4ea2985c7d52bd369194399c529a
Rezaul-Karim-Sujon <rksujon126706@gmail.com>
2021-11-06 21:23:32 +0600

    Configure SignalR

--- ChitChat/ChitChat/Startup.cs
+++ ChitChat/ChitChat/Startup.cs
@@ -1,3 +1,4 @@
+using ChitChat.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace ChitChat
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +52,7 @@ namespace ChitChat
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapHub<ChatHub>("/chatHub");
             });
         }
     }
